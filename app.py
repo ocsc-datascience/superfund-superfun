daf3a64390b37ed6967e3057a3e17d5ec60821e3
Christian Ott <cott@tapir.caltech.edu>
2018-10-17 16:43:48 -0700

    small flask update

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
+#!/usr/bin/env python3
+import sys
 from flask import Flask, render_template,jsonify,request
-
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 
 app = Flask(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/db.sqlite"
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/superfund.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+Base = automap_base(metadata=db.metadata)
+engine = db.get_engine()
+Base.prepare(engine, reflect=True)
+Superfund = Base.classes.superfund
+
+sys.exit()
+
+for mappedclass in Base.classes:
+    print(mappedclass)
+
+
+
+@app.route("/superfund_sites")
+def superfund_sites():
+
+#    db.session.query(
+    
+    md = {}
+    
+    return jsonify(md)
 
 
 @app.route("/")
