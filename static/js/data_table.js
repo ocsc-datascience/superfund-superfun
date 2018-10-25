43f180fd1ecc4cbd13be6549eddca082fa57546a
Jane Chang <jcl_chang@yahoo.com>
2018-10-25 02:14:51 -0700

    updated data table html & js

--- static/js/data_table.js
+++ static/js/data_table.js
@@ -1,20 +1,10 @@
-
-
-/* function customFilter(data, filterParams){
-    //data - the data for the row being filtered
-    //filterParams - params object passed to the filter
-
-    return data.name == "bob" && data.height < filterParams.height; //must return a boolean, true if it passes the filter.
-}
-
-table.setFilter(customFilter, {height:3});
- */
-
-
 //create Tabulator on DOM element with id "superfund-table"
   var table = new Tabulator("#superfund-table", {
- 	height:600, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
- 	layout:"fitColumns", //fit columns to width of table (optional)
+ 	//height:500, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+	//height:"100%",
+	pagination:"local", //enable local pagination.
+    paginationSize:15, // rows per page (default = 10)
+	layout:"fitColumns", //fit columns to width of table (optional)
 	responsiveLayout:"hide",
  	columns:[ //Define Table Columns
 	 	{title:"Site Name", field:"name"},
@@ -24,11 +14,12 @@ table.setFilter(customFilter, {height:3});
 	 	{title:"Zip Code", field:"xzip", align:"left", headerFilter:true},
 	 	{title:"HRS Score", field:"hrs_score", align:"right", sorter:"number"},
  	],
- 	//rowClick:function(e, row){ //trigger an alert message when the row is clicked
- 	//	alert("Row " + row.getData().id + " Clicked!!!!");
- 	//},
 });
-  
+
+
+$("#tabulator-controls  button[name=download]").on("click", function(){
+   	table.download("csv", " Download.csv");
+});
 
 d3.json("/superfund_sites", function(data) {
     //load sample data into the table
